7ebcdbca22ea2e5d8531acd2c3ce3e3efd9cb1ac
Valerian Thibault <valerianderenne@gmail.com>
2020-10-12 16:15:08 +0200

    excercice 2.6 - ok, this one was easy, if i know how to spell

---
+++ control/2.6.js
@@ -0,0 +1,25 @@
+const readlineSync = require("readline-sync");
+
+let question = readlineSync.question("Enter a number between 1 and 7. : ");
+
+if (question==="1") {
+    console.log("Monday");
+}
+else if (question==="2") {
+    console.log("Tuesday");
+}
+else if (question==="3") {
+    console.log("Wednesday");
+}
+else if (question==="4") {
+    console.log("Thursday");
+}
+else if (question==="5") {
+    console.log("Friday");
+}
+else if (question==="6") {
+    console.log("Saturday");
+}
+else if (question==="7") {
+    console.log("Sunday");
+}
