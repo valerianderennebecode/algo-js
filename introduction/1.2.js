445123f6ecc83e2c4cc578f81e908ea0c15386e6
Valerian Thibault <valerianderenne@gmail.com>
2020-10-12 10:35:59 +0200

    exercice2

---
+++ introduction/1.2.js
@@ -0,0 +1,7 @@
+const readlineSync = require("readline-sync");
+
+let name = "Lambert";
+let firstName = "Gerad";
+let city = "Paris";
+
+console.log ('"Your name is ' + firstName + ' ' + name + ' and you live in ' + city +'."')
