11818dbfc7df26151ee1ab771addf3c0de5b7814
Valerian Thibault <valerianderenne@gmail.com>
2020-10-13 10:30:19 +0200

    exercice 3.3 - couldn't do the bonus

---
+++ arrays/3.3.js
@@ -0,0 +1,10 @@
+let arr = ["Apple", "Pear", "Banana"];
+
+// let arrcopy = arr;
+//console.log(arr + " " + arrcopy);
+
+arr.push("Apple");
+arr.push("Pear");
+arr.push("Banana");
+
+console.log(arr);
